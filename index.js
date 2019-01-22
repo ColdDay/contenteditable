ad891b1a6f44933bbdd4220443df0077055911f5
yangjinkai <yangjinkai@yangjinkaideMacBook-Pro.local>
2019-01-22 20:42:33 +0800

    添加停止编辑功能

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-function sendMessage() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  	selectedId = tabs[0].id;
-  	chrome.tabs.executeScript(selectedId, {file: "main.js"});
-  });
+function executeEdit() {
+  chrome.tabs.executeScript({code: "document.body.setAttribute('contenteditable', true)"}, function(){
+    console.log('开始编辑')
+  })
 }
-sendMessage();
+document.getElementById('stopEdit').addEventListener('click', function(){
+  chrome.tabs.executeScript({code: "document.body.setAttribute('contenteditable', false)"}, function(){
+    console.log('停止编辑')
+    window.close();
+  })
+})
+window.onload = function () {
+  executeEdit()
+}
